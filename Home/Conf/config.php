82e559705e15f273195072f961529e009091026b
lyt <920740402@qq.com>
2015-08-03 16:25:42 +0800

    commit php demo

---
+++ Home/Conf/config.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	// 数据库常用配置
+	'DB_TYPE'			=>	'mysql',			// 数据库类型
+
+	'DB_HOST'			=>	'localhost',			// 数据库服务器地址
+	'DB_NAME'			=>	'shop',			// 数据库名
+	'DB_USER'			=>	'root',		// 数据库用户名
+	'DB_PWD'			=>	'root',		// 数据库密码
+
+	'DB_PORT'			=>	3306,				// 数据库端口
+	
+	//开启debug模式
+ 	'APP_DEBUG'         =>  true, 
+    'SHOW_PAGE_TRACE'   =>  true, 
+);
+?>
